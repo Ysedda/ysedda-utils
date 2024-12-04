ac3b2dc197d1e8fc7234a068b61c213c443c72c6
Ysedda <arthur.baudoin@icloud.com>
2024-12-04 22:13:14 +0100

    feat: :wrench: bash aliases file

---
+++ bash_aliases.sh
@@ -0,0 +1,6 @@
+# aliases
+alias db-start="docker start giga-pg"
+alias db-stop="docker stop giga-pg"
+alias db-create="sh ~/ysedda-utils/postgres-docker/create-db.sh"
+alias db-mount="docker compose -f ~/ysedda-utils/postres-docker/docker-compose.yml up -d"
+alias db-unmount="docker compose -f ~/ysedda-utils/postres-docker/docker-compose.yml down"
